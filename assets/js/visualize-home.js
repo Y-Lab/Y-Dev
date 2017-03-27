a5e20287ec00eb22b06a6d6eafcbc54131693d43
iRB Air <irockbunny@outlook.com>
2017-03-27 16:40:24 +0800

    Update

--- assets/js/visualize-home.js
+++ assets/js/visualize-home.js
@@ -1,41 +1,29 @@
-$(document)
-  .ready(function () {
+$(document).ready(function () {
+  // fix menu when passed
+  $('.masthead')
+    .visibility({
+      once: false,
+      onBottomPassed: function () {
+        $('.fixed.menu').transition('fade in');
+      },
+      onBottomPassedReverse: function () {
+        $('.fixed.menu').transition('fade out');
+      }
+    })
+  ;
+  // create sidebar and attach to menu open
+  $('.ui.sidebar').sidebar('attach events', '.toc.item');
 
-    // fix menu when passed
-    $('.masthead')
-      .visibility({
-        once: false,
-        onBottomPassed: function () {
-          $('.fixed.menu').transition('fade in');
-        },
-        onBottomPassedReverse: function () {
-          $('.fixed.menu').transition('fade out');
-        }
-      })
-    ;
-
-    // create sidebar and attach to menu open
-    $('.ui.sidebar')
-      .sidebar('attach events', '.toc.item')
-    ;
-
-  })
-;
+});
 
 // popup wechat qr code
-$('#wechat-qr-code-click')
-  .popup({
-    popup : $('#wechat-qr-code'),
-    on    : 'click'
-  })
-;
+$('#wechat-qr-code-click').popup({
+  popup: $('#wechat-qr-code'),
+  on: 'click'
+});
 
 // copyright
-$('#copyright')
-  .html(
-   function (){
-     var date = new Date();
-     return '&copy; 2011-' +  date.getFullYear() + ' Beijing Y-English Education &amp; Technology Co., Ltd.';
-   }
-  )
-;
+$('#copyright').html(function (){
+  var date = new Date();
+  return '&copy; 2011-' +  date.getFullYear() + ' Beijing Y-English Education &amp; Technology Co., Ltd.';
+});
